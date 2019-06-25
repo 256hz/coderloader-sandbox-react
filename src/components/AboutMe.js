afb5c4a143da750392a486cbc004119f79a426e8
Abe Dolinger <a.d@256hz.com>
2019-06-25 16:33:41 -0700

    Only load section heading if bio isn't loaded yet

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -3,10 +3,9 @@ import SectionHeading from './SectionHeading'
 import { Grid } from 'semantic-ui-react'
 
 const AboutMe = (props) => {
-  let words = props.user.bio.split(" ")
-
-  return (
-    <Grid columns='equal'>
+  if (!props.user.bio) {
+    return (
+      <Grid columns='equal'>
       <SectionHeading text="About Me"
         startEdit={_ => props.startEdit(props.user, 'users')}
         editing={props.editing}
@@ -14,28 +13,36 @@ const AboutMe = (props) => {
         sectionEdit={true}
       user={props.user}
       />
+      </Grid>
+    )
+  } else {
+    let words = props.user.bio.split(" ")
 
-      <Grid.Row columns={16}>
-        <Grid.Column width={2}></Grid.Column>
-        <Grid.Column width={12}>
-          {words.map( word => {
-            return word.includes('http://')
-              ? <a href={word} target="_blank" rel="noopener noreferrer">{word.slice(7,-1) + " "}</a>
-              : word + " "
-          })}
-        </Grid.Column>
-        <Grid.Column width={2}></Grid.Column>
-      </Grid.Row>
-      <br />
-    </Grid>
-  )
+    return (
+      <Grid columns='equal'>
+        <SectionHeading text="About Me"
+          startEdit={_ => props.startEdit(props.user, 'users')}
+          editing={props.editing}
+          loggedIn={props.loggedIn}
+          sectionEdit={true}
+        user={props.user}
+        />
+
+        <Grid.Row columns={16}>
+          <Grid.Column width={2}></Grid.Column>
+          <Grid.Column width={12}>
+            {words.map( word => {
+              return word.includes('http://')
+                ? <a href={word} target="_blank" rel="noopener noreferrer">{word.slice(7,-1) + " "}</a>
+                : word + " "
+            })}
+          </Grid.Column>
+          <Grid.Column width={2}></Grid.Column>
+        </Grid.Row>
+        <br />
+      </Grid>
+    )
+  }
 }
 
 export default AboutMe
-
-      // <Grid.Row columns={1}>
-      //   <Grid.Column className="heading font-size-large">
-      //     Skills
-      //   </Grid.Column>
-      // </Grid.Row>
-      // <br />
